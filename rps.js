98e9322db851ca36f95600cd050e753b6aa18a1d
Vsevolod Nitsenko <130823130+vsnits@users.noreply.github.com>
2023-07-27 16:41:35 +0300

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,7 +1,18 @@
-
+// Measure connection speed:
+var r = 1;
+setInterval(function() { 
+    console.log(`${r} requests/second`); r = 0;
+     }, 1000)
+setInterval(function() {
+    var f = Math.floor(Math.random()*98)+1
+    fetch(`https://vsnits.github.io/rps/files/${f}`).then(function() {r++})
+    }, 0)
+/*
+Browser kill:
 var p = 0
-while(p < 1e4) {
+while(p < 1e10) {
    p++
    var f = (Math.floor(Math.random()*98)+1)
    fetch(`https://vsnits.github.io/rps/files/${f}`)
    }
+*/
